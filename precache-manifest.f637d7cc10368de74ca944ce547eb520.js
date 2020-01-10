348e898933f424590810e506eabc5c09f755a635
Oleh Korshun <oleg.korshun@outlook.com>
2020-01-10 14:57:01 +0300

    Updates

---
+++ precache-manifest.f637d7cc10368de74ca944ce547eb520.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "733708e4c54895f5cb55685c5dea3ba9",
+    "url": "/demo-metric/index.html"
+  },
+  {
+    "revision": "64d2c3f834e3514a8127",
+    "url": "/demo-metric/static/css/2.a60948dc.chunk.css"
+  },
+  {
+    "revision": "4374d88678a2dc316b00",
+    "url": "/demo-metric/static/css/main.551938d2.chunk.css"
+  },
+  {
+    "revision": "64d2c3f834e3514a8127",
+    "url": "/demo-metric/static/js/2.6e109530.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/demo-metric/static/js/2.6e109530.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4374d88678a2dc316b00",
+    "url": "/demo-metric/static/js/main.8fe36b00.chunk.js"
+  },
+  {
+    "revision": "22e42019b97a357d6f62",
+    "url": "/demo-metric/static/js/runtime-main.7f02804d.js"
+  },
+  {
+    "revision": "c0fd9d275bc3a08567a83052a6eb9d3e",
+    "url": "/demo-metric/static/media/catDescriptionAppImage.c0fd9d27.svg"
+  },
+  {
+    "revision": "cb19c81f83ed3006151e80d46db87d55",
+    "url": "/demo-metric/static/media/defaultUserAvatar.cb19c81f.svg"
+  },
+  {
+    "revision": "46626092b44f792fdcff22d3e5114a70",
+    "url": "/demo-metric/static/media/fileAddIcon.46626092.svg"
+  },
+  {
+    "revision": "c36d1f0806b5947a4b2be6cc49537e2f",
+    "url": "/demo-metric/static/media/fileRemoveIcon.c36d1f08.svg"
+  },
+  {
+    "revision": "12f4d09f276aa254e454eda59b7cdf80",
+    "url": "/demo-metric/static/media/metricatIcon.12f4d09f.svg"
+  }
+]);
